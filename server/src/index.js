f72bfaa47c0ab8ec41115cade9a4872184fa830d
rikster <rhounslow@gmail.com>
2021-08-14 15:39:49 +1000

    mocks

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,30 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
 
+const mocks = {
+  Query: () => ({
+    tracksForHome: () => [...new Array(9)],
+  }),
+  Track: () => ({
+    id: () => 'track_01',
+    title: () => 'Astro Kitty, Space Explorer',
+    author: () => {
+      return {
+        name: 'Grumpy Cat',
+        photo:
+          'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg',
+      };
+    },
+    thumbnail: () =>
+      'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
+    length: () => 1210,
+    modulesCount: () => 6,
+  }),
+};
 //TODO
 const server = new ApolloServer({
-  typeDefs
+  typeDefs,
+  mocks,
 });
 
 server.listen().then(() => {
